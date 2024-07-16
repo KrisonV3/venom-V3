b7372a1ef87ae306292da92f046e20909d621330
KrisonV3 <krisonsronghang@gmail.com>
2024-07-16 12:43:50 +0530

    Update venom.py

--- venom.py
+++ venom.py
@@ -10,7 +10,7 @@ MAX_RESTARTS = 5
 RESTART_PERIOD = 300 # Seconds
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-bot = Bot(API_TOKEN)
+bot = Bot(7240385776:AAHLYR0Ey02P9-P20s8rGmi0mNe1OA43wFg)
 
 def start_bot():
     """Start the bot script as a subprocess."""
